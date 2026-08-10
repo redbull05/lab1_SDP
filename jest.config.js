6ba2f3ed3ecd48fc9ccb7451db4a1b31416aeeb4
Nhlamulo <2819916@students.wits.ac.za>
2026-08-11 00:33:31 +0200

    Fixed and made sure that my testing works

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   testEnvironment: 'node',
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
